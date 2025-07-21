0572d618248dd908b6c60771e6106a2c034986e4
zawminkhant <mzaw17591@mail.com>
2025-07-22 00:41:14 +0700

    add googlemap

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,17 +1,32 @@
 import 'package:firstapp1/screen/search/search_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  static const CameraPosition _initialCameraPosition = CameraPosition(
+    target: LatLng(13.7563, 100.5018), // Bangkok for example
+    zoom: 12,
+  );
+  
+  Widget buildMap(){
+    return GoogleMap(
+        initialCameraPosition:_initialCameraPosition,
+      myLocationEnabled: true,
+      zoomControlsEnabled: true,
+      myLocationButtonEnabled: true,
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
           children:[
-            Image.asset('assets/img_2.png',height: 200,fit: BoxFit.cover,),
+            buildMap(),
+            // Image.asset('assets/img_2.png',height: 200,fit: BoxFit.cover,),
             DraggableScrollableSheet(
                 builder: (context, scrollController) => Container(
                   decoration: const BoxDecoration(
@@ -39,7 +54,7 @@ class HomeScreen extends StatelessWidget {
                               prefixIcon: const Icon(Icons.search),
                               fillColor:const Color.fromARGB(255, 213, 209, 209),
                               border:InputBorder.none,
-                              suffixIcon: IconButton(onPressed: (){}, icon: const Icon(Icons.map_sharp)),
+                              suffixIcon: IconButton(onPressed: ()=>Navigator.of(context).push(MaterialPageRoute(builder: (context)=>SearchScreen())), icon: const Icon(Icons.map_sharp)),
                               suffixIconColor: Color.fromARGB(255, 207, 61, 30)
                             ),
                           )
@@ -52,6 +67,7 @@ class HomeScreen extends StatelessWidget {
           ]
       ),
     );
+    
   }
 
 }
--- lib/screen/search/search_screen.dart
+++ lib/screen/search/search_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_place/google_place.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,13 +11,103 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final startController=TextEditingController();
+  final endController=TextEditingController();
+
+  late GooglePlace googlePlace;
+
+  List<AutocompletePrediction> predictions=[];
+  Timer? timer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    String apiKeys='AIzaSyCEZcfkFhjlmixGG7APA1EBFfAkSzjSPb8';
+    googlePlace =GooglePlace(apiKeys);
+  }
+
+  void autoCompleteSearch(String value) async {
+    var result = await googlePlace.autocomplete.get(value);
+    if (result != null && result.predictions != null && mounted) {
+      print(result.predictions!.length);
+      setState(() {
+        predictions = result.predictions!;
+      });
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.white,
+      appBar: AppBar(
+        elevation: 1,
+
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: startController,
+                style: TextStyle(fontSize: 20),
+                decoration:const InputDecoration(
+                    filled: true,
+                    hintStyle: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),
+                    hintText: 'Starting Point',
+                    fillColor:Color.fromARGB(255, 213, 209, 209),
+                    border:InputBorder.none,
+                ),
+                onChanged: (value){
+                    if(value.isNotEmpty){
+                      //return api place result
+                      autoCompleteSearch(value);
+                    }else{
+                      //clear the result
+                    }},
+              ),
+              const SizedBox(height: 10,),
+              TextField(
+                controller: endController,
+                style: TextStyle(fontSize: 20),
+                decoration:InputDecoration(
+                  filled: true,
+                  hintStyle: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),
+                  hintText: 'End Point',
+                  fillColor:const Color.fromARGB(255, 213, 209, 209),
+                  border:InputBorder.none,
+                ),
+                onChanged: (value){
+                  if(timer?.isActive ?? false) timer!.cancel();
+                  timer=Timer(const Duration(milliseconds: 300), (){
+                    if(value.isNotEmpty){
+                      //return api place result
+                      autoCompleteSearch(value);
+                    }else{
+                      //clear the result
+                    }
+                  });
+                },
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: predictions.length,
+                  itemBuilder: (context,index){
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.indigo,
+                      child: Icon(Icons.pin_drop,color: Colors.white,),
+                    ),
+                    title: Text(predictions[index].description.toString()),
+                  );
+              })
+            ],
+          ),
+        ),
+      )
     );
   }
 }
